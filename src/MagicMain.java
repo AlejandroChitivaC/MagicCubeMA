3c885f63c13ad07a2edc4064952955e0bebf8623
Alejandro Chitiva Castillo <83592808+AlejandroChitivaC@users.noreply.github.com>
2022-10-04 23:21:22 -0500

    fixed

--- src/MagicMain.java
+++ src/MagicMain.java
@@ -5,7 +5,7 @@ public class MagicMain {
         int[][] hijo1 = new int[3][3];
         int[][] hijo2 = new int[3][3];
 
-        //Llena los padres con numeros aleatorios del 1 al 9
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 padre1[i][j] = (int) (Math.random() * 9 + 1);
@@ -14,16 +14,27 @@ public class MagicMain {
         }
         //padre 1
 
-        //calcular sumas horizontales y verticales
-
-
         System.out.println("Padre 1");
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(padre1[i][j] + " ");
             }
             System.out.println();
         }
+        int sumac1 = 0;
+        int sumac2 = 0;
+        int sumac3 = 0;
+        for (int i = 0; i < 3; i++) {
+            sumac1 += padre1[i][0];
+            sumac2 += padre1[i][1];
+            sumac3 += padre1[i][2];
+        }
+        System.out.println("col1= "+sumac1);
+        System.out.println("col2= "+sumac2);
+        System.out.println("col3= "+sumac3);
+
+
         for (int i = 0; i < 3; i++) {
             int sumap1 = 0;
             for (int j = 0; j < 3; j++) {
